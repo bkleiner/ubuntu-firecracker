07220eac725e82dcdc142c4eb2af09c427253d5f
Pierre-Yves Aillet <pyaillet@gmail.com>
2024-04-09 10:05:36 +0200

    chore: upgrade to jammy

--- script/image.sh
+++ script/image.sh
@@ -10,7 +10,7 @@ truncate -s 1G /output/image.ext4
 mkfs.ext4 /output/image.ext4
 
 mount /output/image.ext4 /rootfs
-debootstrap --include openssh-server,unzip,rsync,apt,netplan.io,nano focal /rootfs http://archive.ubuntu.com/ubuntu/
+debootstrap --include openssh-server,unzip,rsync,apt,netplan.io,nano jammy /rootfs http://archive.ubuntu.com/ubuntu/
 
 mount --bind / /rootfs/mnt
 chroot /rootfs /bin/bash /mnt/script/provision.sh
@@ -19,5 +19,5 @@ umount /rootfs/mnt
 umount /rootfs
 
 cd /output
-tar czvf ubuntu-focal.tar.gz image.ext4 vmlinux config
+tar czvf ubuntu-jammy.tar.gz image.ext4 vmlinux config
 cd /
